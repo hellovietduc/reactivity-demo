c92c0cb5469116493f2454db447b01124cf441a8
Duc Nguyen <33684291+hellovietduc@users.noreply.github.com>
2025-02-24 00:30:16 +0800

    remove duplicate tests

--- src/2-signal-with-effect.test.ts
+++ src/2-signal-with-effect.test.ts
@@ -82,17 +82,6 @@ const effect = (callback: () => void) => {
 // TESTS
 // -------
 describe('signal with effect', () => {
-  test('returns the initial value', () => {
-    const s = signal(1)
-    expect(s.get()).toBe(1)
-  })
-
-  test('returns the new value after setting', () => {
-    const s = signal(1)
-    s.set(6)
-    expect(s.get()).toBe(6)
-  })
-
   test('calls the effect after setting', () => {
     const s = signal(1)
     const mock = vi.fn()
