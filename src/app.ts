d70a38b76a58a5450d860c0acd5619e8cbff400f
Duc Nguyen <33684291+hellovietduc@users.noreply.github.com>
2025-04-29 19:58:08 +0800

    update 5

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,22 @@
-import { type Component, createApp, effect, signal } from './duck-js'
+import { type Component, computed, createApp, effect, signal } from './duck-js'
 import { DUCK_SVG } from './duck-svg'
 
+// --- State ---
 const numberOfDucks = signal(1)
-const isLuckyNumber = signal(false)
+const isLuckyNumber = computed(() => numberOfDucks.get() % 7 === 0)
 
-const increaseDucks = () => {
-  numberOfDucks.set(numberOfDucks.get() + 1)
-}
+// --- Actions ---
+const increaseDucks = () => numberOfDucks.set(numberOfDucks.get() + 1)
+const decreaseDucks = () =>
+  numberOfDucks.get() > 0 && numberOfDucks.set(numberOfDucks.get() - 1)
+const randomizeDucks = () =>
+  numberOfDucks.set(Math.floor(Math.random() * (50 - 5 + 1)) + 5) // between 5 and 50
 
-const decreaseDucks = () => {
-  const current = numberOfDucks.get()
-  if (current === 0) return
-  numberOfDucks.set(numberOfDucks.get() - 1)
-}
-
-const randomizeDucks = () => {
-  // between 5 and 50
-  numberOfDucks.set(Math.floor(Math.random() * (50 - 5 + 1)) + 5)
-}
-
+// --- Components
 const Ducks: Component = () => {
   effect(() => {
-    if (numberOfDucks.get() % 7 === 0) {
-      isLuckyNumber.set(true)
+    if (isLuckyNumber.get()) {
       window.fireConfetti()
-    } else {
-      isLuckyNumber.set(false)
     }
   })
 
@@ -43,6 +34,7 @@ const Message: Component = () => {
       : ''
 }
 
+// --- Mounting the app
 createApp(Ducks, document.getElementById('ducks'))
 createApp(Message, document.getElementById('msg'))
 
